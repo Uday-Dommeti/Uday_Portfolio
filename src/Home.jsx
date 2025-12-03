f98cf2cb765d34ce786e81d5d0bc0147cd21450b
Uday Dommeti <udaydommeti8@gmail.com>
2025-12-03 15:08:24 +0530

    Image path update

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
 
             <div className="home-image mt-4 mt-md-0" data-aos="fade-left">
                 <img
-                    src="src/Files/Uday PortFolio Pic.jpg"
+                    src="public/assets/Uday-PortFolio-Pic.jpg"
                     alt="Uday"
                     className="rounded-4 shadow-lg home-profile-img"
                     draggable="false"
